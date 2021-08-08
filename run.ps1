7f86dac96c893d77e4fdc90f46e26adca38b4fd9
Ifendeer <functionnotworkingcorrectly@gmail.com>
2021-08-08 12:28:33 -0700

    Fixes for vscode not autocompleting

--- run.ps1
+++ run.ps1
@@ -6,11 +6,19 @@ if($args[0].Count -eq 0){
 }
 
 if (Test-Path -Path $Folder) {
+    if(Test-Path -Path '.vscode'){
+    }else{
+        New-Item '.vscode' -ItemType directory | Out-Null
+        '{"python.autoComplete.extraPaths":["'+$Folder+'"],"python.defaultInterpreterPath": "./'+$Folder+'/Scripts/python.exe","python.analysis.extraPaths": ["'+$Folder+'"]}' | Out-File -FilePath ".\.vscode\settings.json" -Encoding "UTF8"
+    }
     & "$Folder\scripts\activate";
     & "$Folder/scripts/python.exe" -u "`"$PSScriptRoot\$Filename`""
 } else {
+    New-Item '.vscode' -ItemType directory | Out-Null
+    '{"python.autoComplete.extraPaths":["'+$Folder+'"],"python.defaultInterpreterPath": "./'+$Folder+'/Scripts/python.exe","python.analysis.extraPaths": ["'+$Folder+'"]}' | Out-File -FilePath ".\.vscode\settings.json" -Encoding "UTF8"
     py -3 -m venv $Folder
     & "$Folder\scripts\activate"
     & "$Folder/scripts/python.exe" -m pip install --upgrade pip
+    & pip install pygame
     & "$Folder/scripts/python.exe" -u "`"$PSScriptRoot\$Filename`""
 }
