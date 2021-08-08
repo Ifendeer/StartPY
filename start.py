65826c3484688b12d80e72ffb9fc9f8bcc0104f2
Ifendeer <functionnotworkingcorrectly@gmail.com>
2021-08-08 12:22:05 -0700

    Screen now taking events also added icon and title

--- start.py
+++ start.py
@@ -1,10 +1,16 @@
 import pygame
+import pygame.display as wnd_py_disp
 
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+screen = wnd_py_disp.set_mode((800,600))
+wnd_py_disp.set_caption("Testing Game")
+icon = pygame.image.load("book.png")
+wnd_py_disp.set_icon(icon)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            running = False
+            running = False    
+        screen.fill((120,0,0))
+        wnd_py_disp.update()
